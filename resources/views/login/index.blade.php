9cc1d60191e0c22422553424d610ff1fcdf61fdf
MD. Mahidul Islam <32381876+mdmahidulislam@users.noreply.github.com>
2018-08-16 13:34:19 +0600

    update

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<title>Login</title>
-	@include('partials.head')
+	@include('partials.head') -->
 </head>
 <body id="login">
 	<header>
-		@include('partials.header')
+		@include('partials.header') -->
 	</header>
 
 	<div class="container">
